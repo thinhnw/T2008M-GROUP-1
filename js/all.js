7f5edd6b5c7e8fdc1126d55ba891c5afc5b1171a
thinhnw <thinhnv0412@gmail.com>
2020-11-11 21:14:03 +0700

    Add href to 5 articles

--- js/all.js
+++ js/all.js
@@ -5,77 +5,88 @@ let articles = [
 		text: "It can be tough to get into exercise, and once we get into a routine, it can be difficult to allow ourselves a rest day. However, rest days are important.", 
 		image: "images/Signs-your-body-is-telling-you-enough.jpg", 
 		tagName: "healthy", 
-		tag: "healthy living"
+		tag: "healthy living", 
+		src: "signs-body-telling-enough.html"
 	}, 
 	{
 		title: "10 Easy Exercises for Burning Calories",
 		text: "Losing weight comes down to a combination of burning calories through exercise, and cutting them from our diet, these are the best 100 Calorie Burners.", 
 		image: "images/Easy-Exercises-for-Burning-Calories.jpg", 
 		tagName: "fitness", 
-		tag: "fitness"
+		tag: "fitness", 
+		src: "#"
 	}, 
 	{
 		title: "10 Wellness tricks to enhance your mood",
 		text: "Exercise and eating well are the formula for looking good on the outside, but how you feel on the inside is just as important to ensure a healthy and happy lifestyle. Follow our top 10 tips to enhance your mood and ensure a happier, healthier you.", 
 		image: "images/Wellness-Tricks-660x400.jpg", 
 		tagName: "healthy", 
-		tag: "healthy living"
+		tag: "healthy living", 
+		src: "10-wellness-tricks-enhance-mood.html"
 	}, 
 	{
 		title: "If Detoxing Is a Myth – What Can You Do To Get Yourself Healthy?",
 		text: "Despite the endless availability of detoxing products and diet plans, is there actual medical evidence to prove these programmes are effective?", 
 		image: "images/detox-660x400.jpg", 
 		tagName: "healthy", 
-		tag: "healthy living"
+		tag: "healthy living", 
+		src: "#"
 	}, 
 	{
 		title: "10 Yoga & Pilates Poses to Ease Back Pain",
 		text: "Back pain is a common issue which affects many of us on a daily basis. A lack of exercise, bad posture and spending too long sat at a desk are all factors which can cause us to be in pain....", 
 		image: "images/yoga-for-backpain.jpg", 
 		tagName: "yoga", 
-		tag: "yoga"
+		tag: "yoga", 
+		src: "10-yoga-pilates-poses-to-ease-back-pain.html"
 	}, 
 	{
 		title: "The ‘Miracle’ Juice Cleanse – What Does It Really Do To Your Body?",
 		text: "Juicing, juice cleanses, juice diets and their many other incarnations are one of the latest crazes to be taking the health world by storm. But what does it really do to your body?", 
 		image: "images/Miracle-Detox-Juice-660x400.jpg", 
 		tagName: "healthy", 
-		tag: "healthy living"
+		tag: "healthy living", 
+		src: "#"
 	}, 
 	{
 		title: "Juicing Recipes to Support, Not Replace Your Healthy Lifestyle",
 		text: "If you’re stuck for ideas on how to make delicious, healthy juices then you’re in luck thanks to our fantastic juicing recipes.", 
 		image: "images/howto-juicing-recipes-660x400.jpg", 
 		tagName: "recipes", 
-		tag: "recipes"
+		tag: "recipes", 
+		src: "juicing-recipes-healthy-lifestyle.html"
 	}, 
 	{
 		title: "The Ultimate Creatine FAQ: What You Need to Know",
 		text: "Creatine is readily available on most food supplement sites, but is one of the ones that people know the least about. Here’s what you need to know!", 
 		image: "images/creatine-660x400-400x242.jpg", 
 		tagName: "fitness", 
-		tag: "fitness"
+		tag: "fitness", 
+		src: "#"
 	}, 
 	{
 		title: "10 Things You Should Know About Dating Athletic Girls",
 		text: "If it’s the night before a class or a training session, she’s not going to miss it for a late night with you, here’s 10 things you should know about dating athletic girls.", 
 		image: "images/dating-athletic-girls-400x242.jpg", 
 		tagName: "fitness", 
-		tag: "fitness"
+		tag: "fitness", 
+		src: "#"
 	}, 
 	{
 		title: "Uptown Funk Treadmill Dance – Carson Dean",
 		text: "", 
 		image: "images/mark-ronson-uptown-funk-treadmill-dance-400x242.jpg", 
 		tagName: "fun", 
-		tag: "just for fun"
+		tag: "just for fun", 
+		src: "#"
 	}, 
 	{
 		title: "10 Mistakes Women Make in the Gym",
 		text: "When it comes to working out in the gym, there is a lot of conflicting information, we show you the Top 10 mistakes women often make while working out.", 
 		image: "images/woman-treadmill-400x242.jpg", 
 		tagName: "fitness", 
-		tag: "fitness"
+		tag: "fitness", 
+		src: "#"
 	}, 
 ];
 
@@ -84,8 +95,7 @@ function articlesByTag(tagName) {
 	let g_html = "";
 	
 	articles.filter(article => article.tagName === tagName || tagName === "all" ).forEach((article) => {
-		g_html += "" +
-						"<div class=\"article\">" +
+		g_html += "<a class=\"article\" href=\""+ article.src + "\">" +
 							"<div class=\"article-img\">" +
 								"<img src=\""+ article.image +"\" >" +
 								"<div class=\"article-tag " + article.tagName  + "\">" + article.tag + "</div>" +
@@ -98,7 +108,7 @@ function articlesByTag(tagName) {
 									article.text +
 								"</p>" +
 							"</div>" +
-						"</div>";
+					"</a>";
 	})
 	return g_html;
 }
